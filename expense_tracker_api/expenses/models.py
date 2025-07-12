6ef0b3b11e37a49e8c442becdc627c9d882295df
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-07-12 14:30:27 +0530

    update README

--- expense_tracker_api/expenses/models.py
+++ expense_tracker_api/expenses/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE )
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
     category = models.CharField(max_length=50)
     date = models.DateField()
     description = models.CharField(max_length=255,blank=True)
